3988f8d1c38488ba39087a30406fce05c25255ce
Christian Abate-Wong <abatewongc@wit.edu>
2019-10-03 14:53:21 -0400

    lmao

--- src/main/java/oof/oofengine/OofEngineApplication.java
+++ src/main/java/oof/oofengine/OofEngineApplication.java
@@ -2,7 +2,7 @@ package oof.oofengine;
 
 import oof.oofengine.data.Object3D;
 
-public class Main {
+public class OofEngineApplication {
     public static void main(String[] args) {
         Object3D obj = new Object3D();
         obj.render();
