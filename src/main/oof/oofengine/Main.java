4d07a009b446cdbfdbc79fc9015577b03f1da998
Darrien Kennedy <dkennedy@veracode.com>
2019-10-03 14:42:11 -0400

    initial test

--- src/main/oof/oofengine/Main.java
+++ src/main/oof/oofengine/Main.java
@@ -1,7 +1,10 @@
-package main.oof.oofengine;
+package oof.oofengine;
+
+import oof.oofengine.data.Object3D;
 
 public class Main {
     public static void main(String[] args) {
-        Object3D
+        Object3D obj = new Object3D();
+        obj.render();
     }
 }
--- src/main/oof/oofengine/data/Object3D.java
+++ src/main/oof/oofengine/data/Object3D.java
@@ -1,10 +1,13 @@
-package main.oof.oofengine.data;
+package oof.oofengine.data;
+
+import org.lwjgl.glfw.GLFW;
 
 public class Object3D {
     public void render() {
         String title = "";
         long monitor = 1000L;
         long share = 1000L;
+        System.out.printf("REEEE, %s %d %d\n", title, monitor, share);
         GLFW.glfwCreateWindow(400, 400, title, monitor, share);
     }
 }
