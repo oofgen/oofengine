57bb56958fc4ed4a7c5eed99c888577696ad15e2
Darrien Kennedy <dkennedy@veracode.com>
2019-10-04 13:20:52 -0400

    Stinky garbage shader code

--- src/main/java/oof/oofengine/OofEngineApplication.java
+++ src/main/java/oof/oofengine/OofEngineApplication.java
@@ -5,6 +5,7 @@ import oof.oofengine.display.WindowManager;
 import oof.oofengine.renderEngine.Loader;
 import oof.oofengine.renderEngine.RawModel;
 import oof.oofengine.renderEngine.Renderer;
+import oof.oofengine.shaders.StaticShader;
 
 import java.awt.*;
 
@@ -13,30 +14,35 @@ public class OofEngineApplication {
         WindowManager win = new WindowManager();
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
+//        StaticShader shader = new StaticShader();
 
 
         float[] vertices = {
                 -0.5f, 0.5f, 0f,
                 -0.5f, -0.5f, 0f,
                 0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f,
-                -0.5f, 0.5f, 0f
+                0.5f, 0.5f, 0f
         };
-        RawModel model = loader.loadToVAO(vertices);
+        int[] indices ={
+                0,1,3,
+                3,1,2
+        };
+
+        RawModel model = loader.loadToVAO(vertices, indices);
 
 
         int i = 0;
         while (i < 150) {
+//            shader.start();
             renderer.prepare();
             renderer.render(model);
-
             win.updateWindow();
+//            shader.stop();
             i++;
         }
-        loader.cleanUp();
 
+//        shader.cleanUp();
+        loader.cleanUp();
         win.closeWindow();
     }
 }
--- src/main/java/oof/oofengine/renderEngine/Loader.java
+++ src/main/java/oof/oofengine/renderEngine/Loader.java
@@ -7,6 +7,7 @@ import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +16,12 @@ public class Loader {
     private List<Integer> vaos = new ArrayList<Integer>();
     private List<Integer> vbos = new ArrayList<Integer>();
 
-    public RawModel loadToVAO(float[] positions) {
+    public RawModel loadToVAO(float[] positions, int[] indices) {
         int vaoID = createVAO();
+        bindIndicesBuffer(indices);
         storeDataInAttributesList(0, positions);
         unbindVAO();
-        return new RawModel(vaoID, positions.length / 3);
+        return new RawModel(vaoID, indices.length);
 
     }
 
@@ -46,11 +48,27 @@ public class Loader {
         FloatBuffer buffer = storeDataInFloatBuffer(data);
         GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
         GL20.glVertexAttribPointer(attributeNumber, 3, GL11.GL_FLOAT, false, 0, 0);
-        //GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
     }
 
     private void unbindVAO() {
         GL30.glBindVertexArray(0);
+    }
+
+
+    private void bindIndicesBuffer(int[] indices) {
+        int vboID = GL15.glGenBuffers();
+        vbos.add(vboID);
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboID);
+        IntBuffer buffer = storeDataInIntBuffer(indices);
+        GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+    }
+
+    private IntBuffer storeDataInIntBuffer(int[] data) {
+        IntBuffer buffer = BufferUtils.createIntBuffer(data.length);
+        buffer.put(data);
+        buffer.flip();
+        return buffer;
     }
 
     private FloatBuffer storeDataInFloatBuffer(float[] data) {
